72d8f2b3710738c8a23a437f8283c98055fbe5f8
Douglas Santana Costa <douglas.santana1777@gmail.com>
2022-08-30 15:02:23 -0300

    criado o inicio da lógica do programa

---
+++ decimaBinario.js
@@ -0,0 +1,21 @@
+console.log("***Conversor de decimal para binario***")
+numero = 100
+while (numero > 0) {
+
+}
+console.log(numero + "em binário é: " + decBin(numero))
+
+function decBin(numero) {
+    numBinario = " "
+    resto = 0
+    while (numero < 0) {
+        console.log("Erro! valor/número negativo, digite um valor/número positivo")
+        break;
+    }
+    while (numero > 0) {
+        resto = numero % 2
+        a = resto
+        num /= 2
+
+    }
+}
